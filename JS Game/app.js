e3586d2cac7785c3050daae491676e3c44853fd4
Ankit391 <57256498+Ankit391@users.noreply.github.com>
2023-04-02 18:11:42 +0530

    Add files via upload

---
+++ JS Game/app.js
@@ -0,0 +1,114 @@
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+
+//   winning Pattern Array   //
+let winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+//Player 'X' Plays first //
+let xTurn = true;
+let count = 0;
+
+// disable All buttons//
+
+const disableButtons = () => {
+  btnRef.forEach((element) => (element.disabled = true));
+  // enable popup//
+  popupRef.classList.remove("hide");
+};
+
+// enable All buttons (For New game and Restart)//
+
+const enableButtons = () => {
+  btnRef.forEach((element) => {
+    element.innerText = "";
+    element.disabled = false;
+  });
+  // disable popup//
+  popupRef.classList.add("hide");
+};
+
+//This function is executed when a player wins //
+const winFunction = (letter) => {
+  disableButtons();
+  if (letter == "X") {
+    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+  } else {
+    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+  }
+};
+
+// Function for draw //
+
+const drawFunction = () => {
+  disableButtons();
+  msgRef.innerHTML = "&#x1F60E; <br>  it's a draw";
+};
+
+// New Game //
+newgameBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+restartBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+
+//win logic //
+const winChecker = () => {
+  // loop through all win patterns //
+  for (let i of winningPattern) {
+    let [element1, element2, element3] = [
+      btnRef[i[0]].innerText,
+      btnRef[i[1]].innerText,
+      btnRef[i[2]].innerText,
+    ];
+    // check if elements are filled //
+    // if 3 empty elements are same and would give win as would //
+    if (element1 != "" && (element2 != "") & (element3 != "")) {
+      if (element1 == element2 && element2 == element3) {
+        winFunction(element1);
+      }
+    }
+  }
+};
+
+//Display x/o on click //
+btnRef.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (xTurn) {
+      xTurn = false;
+      // Display x //
+      element.innerText = "X";
+      element.disabled = true;
+    } else {
+      xTurn = true;
+      // Display o //
+      element.innerText = "O";
+      element.disabled = true;
+    }
+    // increment count on each click //
+    count += 1;
+    if (count == 9) {
+      drawFunction();
+    }
+    //check for win on every click //
+    winChecker();
+  });
+});
+
+//enable Buttons and disable popup on page load //
+
+window.onload = enableButtons;
